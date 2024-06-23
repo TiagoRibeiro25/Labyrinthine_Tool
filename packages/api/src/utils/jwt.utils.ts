e1e6b43871e868c0fd1dc7d1b189e4f83c68bc01
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-06-24 00:40:46 +0100

    Update jwt.utils.ts

--- packages/api/src/utils/jwt.utils.ts
+++ packages/api/src/utils/jwt.utils.ts
@@ -1,6 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export default {
+	/**
+	 * Utility function for generating a JWT token.
+	 * @param userId - The ID of the user.
+	 * @returns The generated JWT token.
+	 */
 	generateToken: (userId: string): string => {
 		return jwt.sign({ userId }, process.env.JWT_SECRET as string, {
 			expiresIn: "1d",
