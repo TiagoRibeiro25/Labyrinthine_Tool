a876217409cbca988d7e6f54640e4f1fbc2350fa
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-07-01 00:43:42 +0100

    Added missing comments

--- packages/api/src/hooks/handleAdminValidation.hooks.ts
+++ packages/api/src/hooks/handleAdminValidation.hooks.ts
@@ -7,16 +7,25 @@ import utils from "../utils";
 
 //! REQUIRES handleAuthToken.hooks.ts TO RUN BEFORE THIS HOOK
 
+/**
+ * Handles the validation for admin users.
+ *
+ * @param request - The Fastify request object.
+ * @param reply - The Fastify reply object.
+ * @returns A promise that resolves to void.
+ */
 export default async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
 	try {
 		const userId = request.headers["userId"] as string;
 		let isUserAdmin: boolean = false;
 
+		// Check if there's already a cached value for the user's admin status
 		const cachedUser = await db.adminListRedisInstance.get(userId);
 
 		if (cachedUser) {
 			isUserAdmin = cachedUser === "true";
 		} else {
+			// If there's no cached value, fetch the user's admin status from the database
 			const user = await db.main.user.findUnique({
 				where: { id: userId },
 				select: { isAdministrator: true },
@@ -31,6 +40,7 @@ export default async (request: FastifyRequest, reply: FastifyReply): Promise<voi
 			isUserAdmin = user.isAdministrator;
 		}
 
+		// Cache the user's admin status for a day
 		await db.adminListRedisInstance.set(
 			userId,
 			isUserAdmin.toString(),
