b88b062f8eaf391d0843d181d451bf910803b02f
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-07-09 17:45:43 +0100

    chore: Add smooth scrolling behavior to index.css and create DownArrowIcon component

--- packages/web/src/views/Home/Home.tsx
+++ packages/web/src/views/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Fade } from "react-awesome-reveal";
 import Marquee from "react-fast-marquee";
+import DownArrowIcon from "./components/DownArrowIcon/DownArrowIcon";
 import FreeToUseIcon from "./components/FreeToUseIcon/FreeToUseIcon";
 import MainTitleAnimation from "./components/MainTitleAnimation/MainTitleAnimation";
 import OpenSourceIcon from "./components/OpenSourceIcon/OpenSourceIcon";
@@ -33,93 +34,101 @@ const Home: React.FC = (): React.JSX.Element => {
 	}, []);
 
 	return (
-		<div className="flex flex-col items-center h-full pt-10">
-			<MainTitleAnimation
-				delay={MAIN_TITLE_ANIMATION_DELAY}
-				duration={MAIN_TITLE_ANIMATION_DURATION}
-				className="hidden mt-10 text-center duration-500 ease-in-out lg:block"
-			>
-				<h1 className="text-6xl labyrinth-font">Labyrinthine Tool</h1>
-			</MainTitleAnimation>
+		<div className="flex justify-center h-full">
+			<div className="h-full max-w-[985px]">
+				<section className="flex flex-col items-center h-full pt-10" id="welcome">
+					<MainTitleAnimation
+						delay={MAIN_TITLE_ANIMATION_DELAY}
+						duration={MAIN_TITLE_ANIMATION_DURATION}
+						className="hidden mt-10 text-center duration-500 ease-in-out lg:block"
+					>
+						<h1 className="text-6xl labyrinth-font">Labyrinthine Tool</h1>
+					</MainTitleAnimation>
 
-			<div className="block lg:hidden">
-				<Fade
-					direction="up"
-					triggerOnce
-					delay={MAIN_TITLE_ANIMATION_DELAY - 400}
-					duration={MAIN_TITLE_ANIMATION_DURATION}
-					className="mt-10 text-center duration-500 ease-in-out"
-				>
-					<h1 className="text-4xl sm:text-6xl labyrinth-font">Labyrinthine Tool</h1>
-				</Fade>
-			</div>
-
-			{isTitleAnimationOver && (
-				<Fade direction="up" triggerOnce duration={800} className="mt-10">
-					<h2 className="text-2xl">
-						An online tool made to help you with your{" "}
-						<a
-							href="https://store.steampowered.com/app/1302240/Labyrinthine/"
-							target="_blank"
-							rel="noreferrer"
-							className="labyrinth-font hover:underline animate-pulse"
+					<div className="block lg:hidden">
+						<Fade
+							direction="up"
+							triggerOnce
+							delay={MAIN_TITLE_ANIMATION_DELAY - 400}
+							duration={MAIN_TITLE_ANIMATION_DURATION}
+							className="mt-10 text-center duration-500 ease-in-out"
 						>
-							Labyrinthine
-						</a>{" "}
-						cosmetics hunts
-					</h2>
-
-					<Fade direction="up" triggerOnce duration={800} delay={600}>
-						<h3 className="mt-4 text-xl">
-							Never lose track of your cosmetics again, and always know what you need to find
-							to complete your collection
-						</h3>
-					</Fade>
-
-					<div className="xl:w-[985px] mt-20">
-						<Fade direction="up" triggerOnce duration={700} delay={1000}>
-							<h4 className="mt-12 text-xl">
-								Now updated with the latest cosmetics from the{" "}
-								<a
-									href="https://store.steampowered.com/news/app/1302240/view/4141701971813405619"
-									target="_blank"
-									rel="noreferrer"
-									className="labyrinth-font hover:underline"
-								>
-									Summer Event
-								</a>
-								:
-							</h4>
-
-							<div className="flex mt-12 sm:w-auto w-200px">
-								<Marquee
-									className="hidden overflow-hidden sm:flex"
-									autoFill
-									gradient
-									gradientColor="black"
-									gradientWidth={200}
-								>
-									{cosmetics.map((cosmetic: string, index: number) => (
-										<div key={index} className="flex flex-col items-center mr-8">
-											<img
-												src={cosmetic}
-												alt={cosmetic}
-												className="object-cover w-32 h-32 border-2 border-gray-900 rounded-3xl"
-											/>
-										</div>
-									))}
-								</Marquee>
-							</div>
+							<h1 className="text-4xl sm:text-6xl labyrinth-font">Labyrinthine Tool</h1>
 						</Fade>
 					</div>
 
-					<Fade
-						direction="up"
-						triggerOnce
-						duration={800}
-						delay={2000}
-						className="mt-24 text-center"
-					>
+					{isTitleAnimationOver && (
+						<Fade direction="up" triggerOnce duration={800} className="mt-10">
+							<h2 className="text-2xl">
+								An online tool made to help you with your{" "}
+								<a
+									href="https://store.steampowered.com/app/1302240/Labyrinthine/"
+									target="_blank"
+									rel="noreferrer"
+									className="labyrinth-font hover:underline animate-pulse"
+								>
+									Labyrinthine
+								</a>{" "}
+								cosmetics hunts
+							</h2>
+
+							<Fade direction="up" triggerOnce duration={800} delay={600}>
+								<h3 className="mt-4 text-xl">
+									Never lose track of your cosmetics again, and always know what you need to
+									find to complete your collection
+								</h3>
+							</Fade>
+
+							<div className="mt-20">
+								<Fade direction="up" triggerOnce duration={700} delay={1000}>
+									<h4 className="mt-12 text-xl">
+										Now updated with the latest cosmetics from the{" "}
+										<a
+											href="https://store.steampowered.com/news/app/1302240/view/4141701971813405619"
+											target="_blank"
+											rel="noreferrer"
+											className="labyrinth-font hover:underline"
+										>
+											Summer Event
+										</a>
+										:
+									</h4>
+
+									<div className="flex mt-12">
+										<Marquee
+											className="hidden overflow-hidden sm:flex"
+											autoFill
+											gradient
+											gradientColor="black"
+											gradientWidth={200}
+										>
+											{cosmetics.map((cosmetic: string, index: number) => (
+												<div key={index} className="flex flex-col items-center mr-8">
+													<img
+														src={cosmetic}
+														alt={cosmetic}
+														className="object-cover w-32 h-32 border-2 border-gray-900 rounded-3xl"
+													/>
+												</div>
+											))}
+										</Marquee>
+									</div>
+								</Fade>
+
+								<div className="flex justify-center mt-24">
+									<Fade direction="up" triggerOnce duration={500} delay={1700}>
+										<a href="#why-should-you-use">
+											<DownArrowIcon className="w-8 h-8 cursor-pointer opacity-85< animate-bounce" />
+										</a>
+									</Fade>
+								</div>
+							</div>
+						</Fade>
+					)}
+				</section>
+
+				<section id="why-should-you-use">
+					<Fade direction="up" triggerOnce duration={800} className="mt-24 text-center">
 						<h2 className="text-3xl labyrinth-font">Why should you use this tool?</h2>
 
 						<div className="flex flex-row justify-between mb-20 space-x-12">
@@ -128,8 +137,8 @@ const Home: React.FC = (): React.JSX.Element => {
 							<ReasonToUseCard Icon={UpToDateIcon} title="Up to Date" />
 						</div>
 					</Fade>
-				</Fade>
-			)}
+				</section>
+			</div>
 		</div>
 	);
 };
