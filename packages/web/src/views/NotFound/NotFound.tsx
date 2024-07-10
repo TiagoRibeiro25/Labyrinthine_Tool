8b406ec62d359ba402315909e9fb0712ffcc2d2a
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-07-10 13:30:53 +0100

    v

--- packages/web/src/views/NotFound/NotFound.tsx
+++ packages/web/src/views/NotFound/NotFound.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
 
 const NotFound: React.FC = (): React.JSX.Element => {
 	return (
-		<div>
-			<h1 className="mt-20 text-4xl font-bold text-center text-gray-800">Not Found</h1>
-			<Link to="/" className="block mt-5 text-center text-blue-500">
-				Back to Home
-			</Link>
+		<div className="flex flex-col items-center">
+			<Fade triggerOnce direction="up" delay={100} duration={800}>
+				<h1 className="mt-20 text-6xl text-center text-gray-200 labyrinth-font">Oops!</h1>
+			</Fade>
+
+			<Fade triggerOnce direction="up" delay={800} duration={800}>
+				<p className="mt-5 text-center text-gray-200">
+					The page you are looking for does not exist.
+				</p>
+			</Fade>
+
+			<Fade className="mt-12" triggerOnce direction="up" delay={1500} duration={800}>
+				<Link to="/">
+					<Button reversedColors>Back to Home</Button>
+				</Link>
+			</Fade>
 		</div>
 	);
 };
